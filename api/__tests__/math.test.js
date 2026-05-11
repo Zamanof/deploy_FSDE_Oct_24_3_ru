267de2c640af1b25f1f0c27fdda137759bf719ac
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-05-11 21:29:37 +0400

    ci/cd first api unit test

---
+++ api/__tests__/math.test.js
@@ -0,0 +1,10 @@
+function division(a, b){
+    return a/b;
+}
+
+test('10/2 equqals 5', ()=>{
+    expect(division(10, 2)).toBe(5)
+})
+test('15/5 equqals 3', ()=>{
+    expect(division(15, 5)).toBe(3)
+})
